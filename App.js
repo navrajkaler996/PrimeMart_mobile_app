7f84cb112c1c02cb0275fb800e81a1336ec9f4e8
Navraj singh kaler <navrajkaler996@gmail.com>
2024-09-12 15:17:17 -0500

    container for location dropdown created

--- App.js
+++ App.js
@@ -1,7 +1,32 @@
 import { StyleSheet, Text, View } from "react-native";
 import AppNavigator from "./AppNavigator";
+import * as Font from "expo-font";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    const loadFonts = async () => {
+      await Font.loadAsync({
+        "Poppins-Black": require("./assets/fonts/Poppins-Black.ttf"),
+        "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+        "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+      });
+      setFontsLoaded(true);
+    };
+
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return (
+      <View>
+        <Text>Loading Fonts...</Text>
+      </View>
+    );
+  }
+
   return <AppNavigator />;
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,68 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
 import colors from "../constants/color";
+import SearchBar from "../components/SearchBar";
+
+import { Ionicons } from "react-native-vector-icons";
 
 function HomeScreen() {
+  const [searchText, setSearchText] = useState("");
+
   return (
     <View style={styles.container}>
-      <View style={styles["top-container"]}></View>
+      <View style={styles["top-container-1"]}>
+        {/*Search bar */}
+        <SearchBar placeholder="Search groceries..." onSearch={setSearchText} />
+        {/*Location */}
+      </View>
+      <View style={styles["top-container-2"]}>
+        {/*Location */}
+
+        <Text style={styles["location-text"]}>
+          Choose a store for pickup or delivery |{" "}
+          <Text
+            style={{ fontFamily: "Poppins-Black", textTransform: "uppercase" }}>
+            West
+          </Text>
+        </Text>
+        <TouchableOpacity
+          style={styles.iconContainer}
+          //   onPress={() => setDropdownVisible(true)}
+        >
+          <Ionicons name="caret-down" size={24} color="white" />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  "top-container": {
-    height: 300,
+  container: {},
+  "top-container-1": {
+    height: 100,
     backgroundColor: colors.primary,
+
+    justifyContent: "center",
+  },
+  "top-container-2": {
+    height: 50,
+    backgroundColor: "#6969BE",
+
+    justifyContent: "center",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+
+  "location-text": {
+    color: "#ffffff",
+    fontSize: 12,
+    marginLeft: 20,
+    fontFamily: "Poppins-Bold",
+    letterSpacing: 0.4,
+  },
+  iconContainer: {
+    padding: 10,
   },
 });
 
