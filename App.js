c78a05a20743cad9312528957ddb76231cd4153a
Navraj singh kaler <navrajkaler996@gmail.com>
2024-09-12 14:27:56 -0500

    Initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import AppNavigator from "./AppNavigator";
+
+export default function App() {
+  return <AppNavigator />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
